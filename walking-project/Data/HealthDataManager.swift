791e0cddb02fb5a9e35ec6ad0433773531429b48
GgMin <crazy4u5@naver.com>
2023-06-21 22:14:33 +0900

    [DEV] changed to fix viewcontext attempt to recursively call save error

--- walking-project/Data/HealthDataManager.swift
+++ walking-project/Data/HealthDataManager.swift
@@ -250,15 +250,15 @@ func healthDataSync() {
             myWalk.calories = cals
             myWalk.distance = distance
         }
-        
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
+    }
+    
+    do {
+        try viewContext.save()
+    } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nsError = error as NSError
+        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
     }
 }
 
